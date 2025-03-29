5f4983eb5edb709690f870bdd16661cd028aa247
Alvaro Paz da Silva <alanpasi@gmail.com>
2025-03-29 11:52:46 -0300

    Add array access diagram and string iteration examples

--- aula_07-VetoresPonteirosStrings/string.c
+++ aula_07-VetoresPonteirosStrings/string.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
@@ -6,6 +8,14 @@ int main(void) {
 
     /* Ponteiro para vetor char[7] armazenada em sessão .rodata */
     char *pstr = "cabana";
+
+    for (int i = 0; i < sizeof(vstr); i++)
+        printf("vstr[%d] => %c\n", i, vstr[i]);
+
+    puts("===============");
+
+    for (int i = 0; i < strlen(pstr); i++)
+        printf("*(pstr + %d) => %c\n", i, *(pstr + i));
 
     return 0;
 }
